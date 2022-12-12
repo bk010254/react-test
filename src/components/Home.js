2f395a1edc9429a8c0ef320ba735c66a96339fd3
Balaji Kamble <balajikamble1998@gmail.com>
2022-12-12 22:10:37 +1100

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../components/Home.css"
+
+const Home = () => {
+  return (
+    <div className='home_page'>
+    <h1>Home</h1>
+    
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Report.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "../components/Report.css"
+
+const Report = () => {
+  return (
+    <>
+   
+    <h1 className='title'>Areas that are coverd in our Report on Sustainablity</h1>
+   <div class="container">
+ 
+  <main1>Main1</main1>
+  <main2>Main2</main2>
+  <main3>Main3</main3>
+  <div id="sidebar">Environment Pillar</div>
+  <div id="content1">Climate change</div>
+  <div id="content2">Natural Resources</div>
+  <div id="content3">Polution & Waste</div>
+  <div id="content4">Natural Resources</div>
+  <div id="content5">Polution & Waste</div>
+ 
+</div>
+   
+
+
+    </>
+  )
+}
+
+export default Report
